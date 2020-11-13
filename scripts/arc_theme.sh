d213a966f78739e133c5673a1cd05a3945400f2b
thib <thibault@seervision.com>
2020-11-13 23:08:05 +0100

    Add nice looking Gnome shell theme

---
+++ scripts/arc_theme.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Theme
+sudo add-apt-repository ppa:noobslab/themes
+sudo apt-get update
+sudo apt-get install arc-theme
+# Icons
+sudo add-apt-repository ppa:noobslab/icons
+sudo apt-get update
+sudo apt-get install arc-icons
+
+
+# mv OSX-Arc-Shadow-master.zip /usr/share/themes
+
+
