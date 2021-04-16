b0d1294ceabae188d2cf7a1dce9a89c3ae5dcd41
thibthib18 <thibault.nicolas1818@gmail.com>
2021-04-16 15:32:20 +0000

    Refactor config

---
+++ config/zsh/powerline.zsh
@@ -0,0 +1,10 @@
+
+# POWERLEVEL10k theme
+POWERLEVEL9K_MODE=awesome-fontconfig
+POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(vi_mode status time ram)
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(dir_writable dir vcs)
+POWERLEVEL9K_PROMPT_ON_NEWLINE=true
+POWERLEVEL9K_MODE="nerdfont-complete"
+P9KGT_BACKGROUND='light'
+P9KGT_COLORS='light'
+antigen theme romkatv/powerlevel10k powerlevel10k
