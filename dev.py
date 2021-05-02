8f4f99626e5d1584f0c07552244715ff61209034
thib <thib@g.com>
2021-05-02 18:46:08 +0000

    Dev cli tool 1st version

--- dev.py
+++ dev.py
@@ -2,28 +2,60 @@
 import fire
 import docker
 
+api = docker.from_env()
 class Build(object):
 
-  def thib(self):
-    return 'build dev thib image'
+    def base(self):
+        tag = 'thib_base'
+        path = './docker/base'
+        api.images.build(path=path,tag=tag)
+        return 'build dev base image'
 
-  def sv(self):
-    return 'build dev sv image'
-docker build -t dev_thib --build-arg user=thib --build-arg BASE_IMAGE=thib_base:latest .
+    def thib(self):
+        tag = 'dev_thib'
+        path = './docker/dev'
+        build_args={
+                "user": "thib",
+                "BASE_IMAGE": "thib_base:latest"
+        }
+        api.images.build(path=path,tag=tag, buildargs=build_args)
+        return 'build dev thib image'
+
+    def sv(self):
+        tag = 'dev_thib'
+        path = './docker/dev/Dockerfile'
+        build_args={
+                "user": "sv",
+                "BASE_IMAGE": "seervision/development:latest"
+        }
+        api.images.build(path=path,tag=tag, buildargs=build_args)
+        return 'build dev sv image'
 
 class Start(object):
 
-  def thib(self):
-    return 'start dev thib image'
+    def thib(self):
+        api.containers.run(
+                image='dev_thib:latest',
+                name='thib_dev',
+                command='zsh',
+                user='thib',
+                tty=True,
+                detach=True,
+                privileged=True,
+                network_mode='host',
+                )
+        return 'start dev thib image'
 
-  def sv(self):
-    return 'start dev sv image'
+    def sv(self):
+        #  checkout run_dev file from my branch
+        #  call it
+        return 'start dev sv image'
 
-class Pipeline(object):
+class Dev(object):
 
-  def __init__(self):
-    self.build = Build()
-    self.start = Start()
+    def __init__(self):
+        self.build = Build()
+        self.start = Start()
 
 if __name__ == '__main__':
-  fire.Fire(Pipeline)
+    fire.Fire(Dev)
