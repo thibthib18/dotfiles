b90d5177cc447b7070c39a1ccd94f742cd36e73d
thibthib18 <thibault.nicolas1818@gmail.com>
2021-05-02 13:24:36 +0000

    Use thib's defaults

--- run_dev.sh
+++ run_dev.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 # Checkout run dev script from my branch
-cd ~/main
-git checkout thib_dev_image -- docker/run_containers/run_dev.sh
-./docker/run_containers/run_dev.sh
+#cd ~/main
+#git checkout thib_dev_image -- docker/run_containers/run_dev.sh
+#./docker/run_containers/run_dev.sh
+
+docker run -dti \
+  --name thib_dev \
+  --net host \
+  --volume /var/run/docker.sock:/var/run/docker.sock \
+  dev_thib
