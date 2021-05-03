70dd515de02f6713b745d481e4ac697be95eca38
thibthib18 <thibault.nicolas1818@gmail.com>
2021-05-03 16:02:46 +0000

    Deleted now unused scripts

--- docker/base/build_image.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker build -t thib_base .
--- docker/dev/build_image.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#docker build -t dev_thib .
-docker build -t dev_thib --build-arg user=thib --build-arg BASE_IMAGE=thib_base:latest .
