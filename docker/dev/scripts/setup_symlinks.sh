4a0f73485a3637e124ec443c66d4161a24c1d84d
thib <thibault@seervision.com>
2021-07-15 09:44:47 +0200

    Simplify setup_symlinks with script

---
+++ docker/dev/scripts/setup_symlinks.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+mkdir -p ~/.config
+
+ln -s ~/dotfiles/config/nvim ~/.config/nvim
+ln -s ~/dotfiles/config/tmux ~/.config/tmux
+ln -s ~/dotfiles/config/zsh ~/.config/zsh
+
+ln -s ~/dotfiles/config/zsh/zshrc ~/.zshrc
+
+rm -f ~/.tmux.conf
+ln -s ~/dotfiles/config/tmux/tmux.conf ~/.tmux.conf
+
+mkdir -p ~/.ipython/profile_default
+ln -s ~/dotfiles/config/ipython/ipython_config.py ~/.ipython/profile_default/ipython_config.py
+
+mkdir -p ~/.local/share/nvim/plugged
+ln -s ~/.local/share/nvim/plugged ~/plugged
+
