dff7d6ddf0a0f7a7b95f4ca5126d95e13a9ed1b5
thib <thib@mail.com>
2021-08-25 13:40:45 +0000

    treesitter textobjects config

--- config/nvim/lua/thib/nvim-treesitter.lua
+++ config/nvim/lua/thib/nvim-treesitter.lua
@@ -4,5 +4,73 @@ require "nvim-treesitter.configs".setup {
     },
     incremental_selection = {
         enable = true
+    },
+    playground = {
+        enable = true,
+        disable = {},
+        updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+        persist_queries = false, -- Whether the query persists across vim sessions
+        keybindings = {
+            toggle_query_editor = "o",
+            toggle_hl_groups = "i",
+            toggle_injected_languages = "t",
+            toggle_anonymous_nodes = "a",
+            toggle_language_display = "I",
+            focus_language = "f",
+            unfocus_language = "F",
+            update = "R",
+            goto_node = "<cr>",
+            show_help = "?"
+        }
+    },
+    textobjects = {
+        select = {
+            enable = true,
+            -- Automatically jump forward to textobj, similar to targets.vim
+            lookahead = true,
+            keymaps = {
+                -- You can use the capture groups defined in textobjects.scm
+                ["af"] = "@function.outer",
+                ["if"] = "@function.inner",
+                ["ac"] = "@class.outer",
+                ["ic"] = "@class.inner",
+                -- Or you can define your own textobjects like this
+                ["iF"] = {
+                    python = "(function_definition) @function",
+                    cpp = "(function_definition) @function",
+                    c = "(function_definition) @function"
+                }
+            }
+        },
+        swap = {
+            enable = true,
+            swap_next = {
+                ["<leader>a"] = "@parameter.inner"
+            },
+            swap_previous = {
+                ["<leader>A"] = "@parameter.inner"
+            }
+        },
+        move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            lookahead = true,
+            goto_next_start = {
+                ["]]"] = "@function.outer",
+                ["]c"] = "@class.outer"
+            },
+            goto_next_end = {
+                ["]["] = "@function.outer",
+                ["]C"] = "@class.outer"
+            },
+            goto_previous_start = {
+                ["[["] = "@function.outer",
+                ["[c"] = "@class.outer"
+            },
+            goto_previous_end = {
+                ["[C"] = "@class.outer",
+                ["[]"] = "@function.outer"
+            }
+        }
     }
 }
