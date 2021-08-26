94711c0703709d2d353c5ee3aa7bbc22a67d2310
thibthib18 <thibault.nicolas1818@gmail.com>
2021-08-26 20:04:37 +0000

    setup nvim plugins

--- docker/dev/scripts/nvim/setup.sh
+++ docker/dev/scripts/nvim/setup.sh
@@ -10,3 +10,6 @@ pip3 install -U jedi
 # Lua lsp
 ~/dotfiles/docker/dev/scripts/nvim/lua-language-server/install.sh
 
+nvim --headless +"CocUpdateSync" +qa #
+nvim --headless +"TSInstall lua cpp python typescript css bash dockerfile graphql tsx" +qa #
+nvim --headless +"PlugInstall" +qa #
