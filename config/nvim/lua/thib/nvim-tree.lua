053a1f1b5b68982bdc3beae2c9cd9c190e66e5df
thib <thib@git.com>
2021-08-30 12:54:08 +0000

    add change dir functions

--- config/nvim/lua/thib/nvim-tree.lua
+++ config/nvim/lua/thib/nvim-tree.lua
@@ -6,7 +6,21 @@ local set_cwd_main = function()
     set_cwd("~/main")
 end
 
+local set_cwd_current_file = function()
+    path = path or vim.fn.expand("%:p:h")
+    set_cwd(path)
+end
+
+local tros = require("thib.ros")
+
+local set_cwd_current_package = function()
+    pkg_name = tros.get_current_package_name()
+    pkg_path = vim.fn.system("rospack find " .. pkg_name)
+    set_cwd(pkg_path)
+end
+
 return {
-    set_cwd = set_cwd,
-    set_cwd_main = set_cwd_main
+    set_cwd_current_file = set_cwd_current_file,
+    set_cwd_main = set_cwd_main,
+    set_cwd_current_package = set_cwd_current_package
 }
