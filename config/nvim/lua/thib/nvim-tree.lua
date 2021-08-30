2cfb7019112b0c619444d5e42bad996425dbbc6c
thibthib18 <thibault.nicolas1818@gmail.com>
2021-08-30 12:29:38 +0000

    add set_cwd functions

--- config/nvim/lua/thib/nvim-tree.lua
+++ config/nvim/lua/thib/nvim-tree.lua
@@ -1,4 +1,12 @@
-local M = {}
-M.set_cwd = function(folder_name)
+local set_cwd = function(folder_name)
     require("nvim-tree.lib").change_dir(folder_name)
 end
+
+local set_cwd_main = function()
+    set_cwd("~/main")
+end
+
+return {
+    set_cwd = set_cwd,
+    set_cwd_main = set_cwd_main
+}
