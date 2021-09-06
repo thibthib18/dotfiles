e0f5e4e38a8dad89ad687b87cccb956863596bc5
thib <thibault@seervision.com>
2021-09-06 15:40:32 +0000

    remove unused IP

--- config/zsh/ros.zsh
+++ config/zsh/ros.zsh
@@ -3,5 +3,4 @@
 source /opt/ros/melodic/setup.zsh
 export ROSCONSOLE_FORMAT='[${severity}] [${node}] [${time}]: ${message}'
 alias rosvar="echo ROS_MASTER_URI=$ROS_MASTER_URI && echo ROS_IP=$ROS_IP && echo ROS_HOSTNAME=$ROS_HOSTNAME"
-export ROS_MASTER_URI='http://129.132.28.163:11311'
 
