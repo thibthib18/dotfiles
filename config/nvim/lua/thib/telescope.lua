8b45cdea25c0b918ad753bc300a0ef879d183170
thib <thibault@seervision.com>
2021-10-05 12:48:20 +0200

    moved to ros plugin

--- config/nvim/lua/thib/telescope.lua
+++ config/nvim/lua/thib/telescope.lua
@@ -50,34 +50,4 @@ M.search_frontend = function()
     )
 end
 
-M.search_package = function()
-    local package_path = tros.get_current_package_path()
-    local package_name = tros.get_current_package_name()
-    if package_path == nil then
-        vim.notify("Not in a ROS package", "error", {title = "Search ROS package"})
-        return
-    end
-    package_path, _ = string.gsub(package_path, "/home/sv/catkin_ws/src", "~/main")
-    require("telescope.builtin").find_files(
-        {
-            prompt_title = "< " .. package_name .. " >",
-            cwd = package_path
-        }
-    )
-end
-
-M.grep_package = function()
-    local package_path = tros.get_current_package_path()
-    if package_path == nil then
-        vim.notify("Not in a ROS package", "error", {title = "Grep ROS package"})
-        return
-    end
-    package_path, _ = string.gsub(package_path, "/home/sv/catkin_ws/src", "~/main")
-    require("telescope.builtin").live_grep(
-        {
-            search_dirs = {package_path}
-        }
-    )
-end
-
 return M
