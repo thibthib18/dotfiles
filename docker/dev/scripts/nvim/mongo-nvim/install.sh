28bf11ed12a39e56ff41fc97968f352cf313139d
thib <thibault@seervision.com>
2021-10-09 17:50:06 +0000

    install mongo dependencies

---
+++ docker/dev/scripts/nvim/mongo-nvim/install.sh
@@ -0,0 +1,19 @@
+
+# Install luarocks
+sudo apt -y install luarocks
+
+# Install dependencies for mongo C driver
+sudo apt -y install libzstd-dev libssl-dev
+
+# Install Mongo C driver
+git clone https://github.com/mongodb/mongo-c-driver.git
+cd mongo-c-driver
+python build/calc_release_version.py > VERSION_CURRENT
+mkdir cmake-build
+cd cmake-build
+cmake -DENABLE_AUTOMATIC_INIT_AND_CLEANUP=OFF ..
+make
+sudo make install
+
+# Install Mongo C driver
+luarocks install lua-mongo
