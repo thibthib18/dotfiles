9dca58e10d82a52768be02c49a21a01d09b4fb7f
thib <thibault@seervision.com>
2021-12-07 20:56:46 +0100

    update ros config

--- config/nvim/lua/thib/ros.lua
+++ config/nvim/lua/thib/ros.lua
@@ -1,7 +1,12 @@
 local catkin_make = require("ros-nvim.build").catkin_make
 local utils = require("ros-nvim.vim-utils")
 
-require("ros-nvim").setup({})
+require("ros-nvim").setup(
+    {
+        catkin_ws_link_from = "/home/sv/catkin_ws/src",
+        catkin_ws_link_to = "~/main"
+    }
+)
 
 local function generate_compile_commands()
     catkin_make("_all_debug", "-DCMAKE_EXPORT_COMPILE_COMMANDS=YES")
