de069bcecdfbdfeb63cfab4fa581a1718352a578
thib <thibault@seervision.com>
2021-12-15 11:27:08 +0000

    update nvim to 0.6

--- docker/dev/scripts/nvim/install_nvim.sh
+++ docker/dev/scripts/nvim/install_nvim.sh
@@ -2,8 +2,8 @@
 
 
 function install_neovim {
-    # Install Neovim > 0.5.0
-    wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
+    # Install Neovim 0.6.0
+    wget https://github.com/neovim/neovim/releases/download/v0.6.0/nvim-linux64.tar.gz
     sudo mv nvim-linux64.tar.gz /opt
     cd /opt/
     sudo tar -xf nvim-linux64.tar.gz
