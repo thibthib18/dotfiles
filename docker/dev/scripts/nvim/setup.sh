bb6e7cf5fc9862568e2900291eb5159be8801b24
thib <thibault@seervision.com>
2022-02-09 17:42:51 +0100

    no need to build sumneko myself anymore

--- docker/dev/scripts/nvim/setup.sh
+++ docker/dev/scripts/nvim/setup.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-
 # Add python2 support
 python2 -m pip install --user --upgrade pynvim
 
 # For Jedi's python completion: install jedi package
 pip3 install -U jedi
 
-# Lua lsp
-~/dotfiles/docker/dev/scripts/nvim/lua-language-server/install.sh
