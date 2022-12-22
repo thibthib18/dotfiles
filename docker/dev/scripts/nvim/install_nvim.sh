6cfd53b0db8b7673d1f75a2d3972405b5a16adcb
thib <thibault@seervision.com>
2022-12-22 11:38:51 +0100

    install from source for Ubuntu 18.04

--- docker/dev/scripts/nvim/install_nvim.sh
+++ docker/dev/scripts/nvim/install_nvim.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
+function install_from_source(){
+  # Install deps
+  sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
+  # Build latest stable
+  git clone https://github.com/neovim/neovim
+  cd neovim
+  git checkout stable
+  make CMAKE_BUILD_TYPE=RelWithDebInfo CMAKE_INSTALL_PREFIX=/opt
+  sudo make install
+}
 
-function install_neovim {
-    # Install Neovim 0.6.0
+function install_release(){
     wget https://github.com/neovim/neovim/releases/download/v0.8.1/nvim-linux64.tar.gz
     sudo mv nvim-linux64.tar.gz /opt
     cd /opt/
     sudo tar -xf nvim-linux64.tar.gz
-    pip3 install neovim
+}
+
+function install_neovim {
+  if [[ $(lsb_release -rs) == "18.04" ]]; then
+    install_from_source
+  else
+    install_release
+  fi
+  pip3 install neovim
 }
 
 function run_packer_install {
