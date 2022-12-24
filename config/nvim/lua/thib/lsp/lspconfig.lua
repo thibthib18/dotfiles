09fd887fbc3285d91fb8f5701979006d2fa0fdd7
thib <thibault@seervision.com>
2022-12-24 08:20:27 +0100

    better lsp mappings

--- config/nvim/lua/thib/lsp/lspconfig.lua
+++ config/nvim/lua/thib/lsp/lspconfig.lua
@@ -22,7 +22,7 @@ M.on_attach = function(client, bufnr)
   local bufopts = { noremap = true, silent = true, buffer = bufnr }
   vim.keymap.set('n', '<space>gD', vim.lsp.buf.declaration, bufopts)
   vim.keymap.set('n', '<space>gd', vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', '<space>gr', vim.lsp.buf.references, bufopts)
+  -- vim.keymap.set('n', '<space>gr', require('telescope.builtin').lsp_references, bufopts)
   -- vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
   vim.keymap.set('n', '<space>gi', vim.lsp.buf.implementation, bufopts)
   -- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
@@ -33,7 +33,7 @@ M.on_attach = function(client, bufnr)
     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
   end, bufopts)
   vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set('n', '<space>rr', vim.lsp.buf.rename, bufopts)
+  -- vim.keymap.set('n', '<space>rr', vim.lsp.buf.rename, bufopts)
   vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
 end
 
--- config/nvim/lua/thib/lsp/lspsaga.lua
+++ config/nvim/lua/thib/lsp/lspsaga.lua
@@ -12,13 +12,13 @@ saga.init_lsp_saga({
 -- if there is no implement it will hide
 -- when you use action in finder like open vsplit then you can
 -- use <C-t> to jump back
-keymap("n", "gh", "<cmd>Lspsaga lsp_finder<CR>", { silent = true })
+keymap("n", "gr", "<cmd>Lspsaga lsp_finder<CR>", { silent = true })
 
 -- Code action
 keymap({ "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<CR>", { silent = true })
 
 -- Rename
-keymap("n", "gr", "<cmd>Lspsaga rename<CR>", { silent = true })
+keymap("n", "<leader>rr", "<cmd>Lspsaga rename<CR>", { silent = true })
 
 -- Peek Definition
 -- you can edit the definition file in this flaotwindow
