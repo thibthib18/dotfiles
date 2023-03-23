e25ef357310717bdbea9ba5d191c372ea9428395
thib <thibault@seervision.com>
2023-03-23 19:41:48 +0400

    set containers config

--- config/nvim/lua/thib/mongo.lua
+++ config/nvim/lua/thib/mongo.lua
@@ -1,22 +1,25 @@
-
-
 require("mongo-nvim").setup(
-    {
-        list_document_key = {
-            seervision = {
-                settings = "setting",
-                ptu_configs = "name",
-                ptu_drivers = "ptu_driver_type",
-                lens_mappings = function(document)
-                    return document.camera_name .. "/" .. document.lens_name
-                end,
-                ptus = "label",
-                analytics = "name"
-            }
-        },
-        delete_document_mapping = "<c-d>"
-    }
+  {
+    list_document_key = {
+      seervision = {
+        settings = "setting",
+        ptu_configs = "name",
+        ptu_drivers = "ptu_driver_type",
+        lens_mappings = function(document)
+          return document.camera_name .. "/" .. document.lens_name
+        end,
+        ptus = "label",
+        analytics = "name",
+        containers = function(document)
+          return vim.json.decode(document.value).name
+        end
+      }
+    },
+    delete_document_mapping = "<c-x>"
+  }
 )
 
 local opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<leader>dbl', function() require('mongo-nvim.telescope.pickers').collection_picker('seervision') end , opts)
+vim.keymap.set('n', '<leader>dbl', function() require('mongo-nvim.telescope.pickers').collection_picker('seervision') end
+  ,
+  opts)
