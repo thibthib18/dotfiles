762ff1180fcb79f8724c7a3e547a586a865b89b1
thib <thibault@seervision.com>
2023-03-23 11:44:27 +0400

    format

--- config/nvim/lua/thib/packer.lua
+++ config/nvim/lua/thib/packer.lua
@@ -263,21 +263,21 @@ return require("packer").startup(
     use "shaunsingh/nord.nvim"
 
     -- REPL
-    use { 'hkupty/iron.nvim'}
+    use { 'hkupty/iron.nvim' }
 
     -- Tab out of (), needs treesitter
     use {
       "abecodes/tabout.nvim",
       config = function()
         require("tabout").setup {
-          tabkey = "<Tab>",             -- key to trigger tabout, set to an empty string to disable
+          tabkey = "<Tab>", -- key to trigger tabout, set to an empty string to disable
           backwards_tabkey = "<S-Tab>", -- key to trigger backwards tabout, set to an empty string to disable
-          act_as_tab = true,            -- shift content if tab out is not possible
-          act_as_shift_tab = false,     -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
-          default_tab = "<C-t>",        -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
-          default_shift_tab = "<C-d>",  -- reverse shift default action,
-          enable_backwards = true,      -- well ...
-          completion = true,            -- if the tabkey is used in a completion pum
+          act_as_tab = true, -- shift content if tab out is not possible
+          act_as_shift_tab = false, -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
+          default_tab = "<C-t>", -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
+          default_shift_tab = "<C-d>", -- reverse shift default action,
+          enable_backwards = true, -- well ...
+          completion = true, -- if the tabkey is used in a completion pum
           tabouts = {
             { open = "'", close = "'" },
             { open = '"', close = '"' },
