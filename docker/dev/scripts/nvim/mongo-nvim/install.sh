c2fbb70331005d1eb1e707191be3ea4d5d1cb133
thib <thibault@seervision.com>
2023-03-23 19:41:31 +0400

    fix lua-mongo install

--- docker/dev/scripts/nvim/mongo-nvim/install.sh
+++ docker/dev/scripts/nvim/mongo-nvim/install.sh
@@ -14,11 +14,13 @@ cd ../..
 rm -rf mongo-c-driver
 
 # Install lua mongo driver
-sudo apt -y install lua5.1
+sudo apt -y install lua5.1 liblua5.1-0-dev luarocks
 git clone https://github.com/neoxic/lua-mongo.git
 cd lua-mongo
 cmake -D USE_LUA_VERSION=5.1
 make
 make install
+# Install somehow doesn't do this correctly?
+cp mongo.so /usr/local/lib/lua/5.1/mongo.so
 cd ..
 rm -rf lua-mongo
