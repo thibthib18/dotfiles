3f13d1a05c266c6bcb9bcedc72816e41d9668bf8
thib <thibault@seervision.com>
2023-03-23 11:54:10 +0400

    replace deprecated sumneko-lua

--- config/nvim/lua/thib/lsp/lua.lua
+++ config/nvim/lua/thib/lsp/lua.lua
@@ -3,30 +3,32 @@ local on_attach = lspconfig.on_attach
 local lsp_flags = lspconfig.lsp_flags
 local capabilities = require('thib.lsp.complete')
 
-require('lspconfig')['sumneko_lua'].setup{
-    capabilities = capabilities,
-    on_attach = on_attach,
-    flags = lsp_flags,
-    -- Server-specific settings...
-    settings = {
-      Lua = {
-        runtime = {
-          -- Tell the language server which version of Lua you're using (LuaJIT in the case of Neovim)
-          version = 'LuaJIT',
-          -- Setup your lua path
-          path = vim.split(package.path, ';'),
-        },
-        diagnostics = {
-          -- Get the language server to recognize the `vim` global
-          globals = {'vim'},
-        },
-        workspace = {
-          -- Make the server aware of Neovim runtime files
-          library = {
-            [vim.fn.expand('$VIMRUNTIME/lua')] = true,
-            [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
-          },
-        },
+
+require('lspconfig').lua_ls.setup {
+  capabilities = capabilities,
+  on_attach = on_attach,
+  flags = lsp_flags,
+  -- Server-specific settings...
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = { 'vim' },
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+        checkThirdParty = false,
+
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
       },
     },
+  },
 }
