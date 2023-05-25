01385e7f0c7437545a436ba9b757ff46b2dde91d
thib <thibault@seervision.com>
2023-05-25 07:24:36 +0000

    update ros version

--- config/zsh/ros.zsh
+++ config/zsh/ros.zsh
@@ -1,6 +1,6 @@
 
 # Source ROS setup
-source /opt/ros/melodic/setup.zsh
+source /opt/ros/noetic/setup.zsh
 export ROSCONSOLE_FORMAT='[${severity}] [${node}] [${time}]: ${message}'
 alias rosvar="echo ROS_MASTER_URI=$ROS_MASTER_URI && echo ROS_IP=$ROS_IP && echo ROS_HOSTNAME=$ROS_HOSTNAME"
 
