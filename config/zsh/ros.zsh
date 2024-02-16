5b141fb74fb4f44f814f4f991cecf9bd4a5b2576
thib <thibault@seervision.com>
2024-02-16 05:56:08 +0000

    add auto cd catkin make

--- config/zsh/ros.zsh
+++ config/zsh/ros.zsh
@@ -4,3 +4,9 @@ source /opt/ros/noetic/setup.zsh
 export ROSCONSOLE_FORMAT='[${severity}] [${node}] [${time}]: ${message}'
 alias rosvar="echo ROS_MASTER_URI=$ROS_MASTER_URI && echo ROS_IP=$ROS_IP && echo ROS_HOSTNAME=$ROS_HOSTNAME"
 
+
+function auto_cd_catkin_make {
+  (cd ~/catkin_ws; catkin_make $@)
+}
+
+alias cm="auto_cd_catkin_make"
