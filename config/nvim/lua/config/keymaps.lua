b1458a9eae3255fe2f797cad1e7b6b04132cf607
thib <thibault@seervision.com>
2024-02-26 17:38:30 +0400

    cleanup keymaps

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -4,11 +4,9 @@
 --
 --
 
-print("Loading keymaps.lua...")
 vim.keymap.del("n", "<leader>ft")
 vim.keymap.del("n", "<leader>fT")
 vim.keymap.del("n", "n")
-vim.keymap.set('n','n', {noremap = true, silent = true})
 
 -- Buffer navigation
 --
@@ -17,30 +15,3 @@ vim.keymap.set('n','n', {noremap = true, silent = true})
 -- delete buffer and go to previous one
 -- move buffer in line
 --
--- vim.keymap.del("n", "<S-l>")
--- vim.keymap.del("n", "<S-h>")
--- vim.keymap.del("n", "<S-j>")
--- vim.keymap.del("n", "<S-k>")
---
--- local opts = { noremap = true, silent = true }
--- vim.keymap.set({ "n", "v", "x" }, "<S-k>", "<cmd>BufferLineCycleNext<cr>", { desc = "Next buffer", remap = true })
-
--- Typescript
--- ok with extras
--- C++
--- to test
--- Python OK
-
--- camel case words
-
--- Session management
--- save
--- load
-
--- Telescope
--- grep
--- customs
--- find files in project
--- find files local
-
--- Treesitter function/class navigation
