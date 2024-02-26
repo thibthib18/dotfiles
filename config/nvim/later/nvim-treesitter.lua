95ba7a3c9c04533fd838914ea93fbc17d4926f38
thib <thibault@seervision.com>
2024-02-26 19:06:48 +0400

    add yaml (and style)

--- config/nvim/later/nvim-treesitter.lua
+++ config/nvim/later/nvim-treesitter.lua
@@ -1,149 +1,149 @@
-require "nvim-treesitter.configs".setup {
-    -- A list of parser names, or "all"
-    ensure_installed = {"c", "lua","vim", "rust", "python", "typescript", "css", "bash", "dockerfile", "graphql", "tsx"},
-    -- Install parsers synchronously (only applied to `ensure_installed`)
-    sync_install = false,
-    -- Automatically install missing parsers when entering buffer
-    auto_install = true,
-    highlight = {
-        enable = true
+require("nvim-treesitter.configs").setup({
+  -- A list of parser names, or "all"
+  ensure_installed = { "c", "lua", "vim", "rust", "python", "typescript", "css", "bash", "dockerfile", "graphql", "yaml", "tsx" },
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+  -- Automatically install missing parsers when entering buffer
+  auto_install = true,
+  highlight = {
+    enable = true,
+  },
+  incremental_selection = {
+    enable = true,
+  },
+  playground = {
+    enable = true,
+    disable = {},
+    updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+    persist_queries = false, -- Whether the query persists across vim sessions
+    keybindings = {
+      toggle_query_editor = "o",
+      toggle_hl_groups = "i",
+      toggle_injected_languages = "t",
+      toggle_anonymous_nodes = "a",
+      toggle_language_display = "I",
+      focus_language = "f",
+      unfocus_language = "F",
+      update = "R",
+      goto_node = "<cr>",
+      show_help = "?",
     },
-    incremental_selection = {
-        enable = true
+  },
+  textobjects = {
+    select = {
+      enable = true,
+      -- Automatically jump forward to textobj, similar to targets.vim
+      lookahead = true,
+      keymaps = {
+        -- You can use the capture groups defined in textobjects.scm
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = "@class.inner",
+      },
     },
-    playground = {
-        enable = true,
-        disable = {},
-        updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-        persist_queries = false, -- Whether the query persists across vim sessions
-        keybindings = {
-            toggle_query_editor = "o",
-            toggle_hl_groups = "i",
-            toggle_injected_languages = "t",
-            toggle_anonymous_nodes = "a",
-            toggle_language_display = "I",
-            focus_language = "f",
-            unfocus_language = "F",
-            update = "R",
-            goto_node = "<cr>",
-            show_help = "?"
-        }
+    swap = {
+      enable = true,
+      swap_next = {
+        ["<leader>as"] = "@parameter.inner",
+      },
+      swap_previous = {
+        ["<leader>As"] = "@parameter.inner",
+      },
     },
-    textobjects = {
-        select = {
-            enable = true,
-            -- Automatically jump forward to textobj, similar to targets.vim
-            lookahead = true,
-            keymaps = {
-                -- You can use the capture groups defined in textobjects.scm
-                ["af"] = "@function.outer",
-                ["if"] = "@function.inner",
-                ["ac"] = "@class.outer",
-                ["ic"] = "@class.inner"
-            }
-        },
-        swap = {
-            enable = true,
-            swap_next = {
-                ["<leader>as"] = "@parameter.inner"
-            },
-            swap_previous = {
-                ["<leader>As"] = "@parameter.inner"
-            }
-        },
-        move = {
-            enable = true,
-            set_jumps = true, -- whether to set jumps in the jumplist
-            lookahead = true,
-            goto_next_start = {
-                ["]]"] = "@function.outer",
-                ["]c"] = "@class.outer"
-            },
-            goto_next_end = {
-                ["]["] = "@function.outer",
-                ["]C"] = "@class.outer"
-            },
-            goto_previous_start = {
-                ["[["] = "@function.outer",
-                ["[c"] = "@class.outer"
-            },
-            goto_previous_end = {
-                ["[C"] = "@class.outer",
-                ["[]"] = "@function.outer"
-            }
-        }
-    }
-}
+    move = {
+      enable = true,
+      set_jumps = true, -- whether to set jumps in the jumplist
+      lookahead = true,
+      goto_next_start = {
+        ["]]"] = "@function.outer",
+        ["]c"] = "@class.outer",
+      },
+      goto_next_end = {
+        ["]["] = "@function.outer",
+        ["]C"] = "@class.outer",
+      },
+      goto_previous_start = {
+        ["[["] = "@function.outer",
+        ["[c"] = "@class.outer",
+      },
+      goto_previous_end = {
+        ["[C"] = "@class.outer",
+        ["[]"] = "@function.outer",
+      },
+    },
+  },
+})
 
-require "treesitter-context".setup {
-    enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-    max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
-    trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-    min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
-    patterns = {
-        -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
-        -- For all filetypes
-        -- Note that setting an entry here replaces all other patterns for this entry.
-        -- By setting the 'default' entry below, you can control which nodes you want to
-        -- appear in the context window.
-        default = {
-            "class",
-            "function",
-            "method",
-            "for",
-            "while",
-            "if",
-            "switch",
-            "case"
-        },
-        -- Patterns for specific filetypes
-        -- If a pattern is missing, *open a PR* so everyone can benefit.
-        tex = {
-            "chapter",
-            "section",
-            "subsection",
-            "subsubsection"
-        },
-        rust = {
-            "impl_item",
-            "struct",
-            "enum"
-        },
-        scala = {
-            "object_definition"
-        },
-        vhdl = {
-            "process_statement",
-            "architecture_body",
-            "entity_declaration"
-        },
-        markdown = {
-            "section"
-        },
-        elixir = {
-            "anonymous_function",
-            "arguments",
-            "block",
-            "do_block",
-            "list",
-            "map",
-            "tuple",
-            "quoted_content"
-        },
-        json = {
-            "pair"
-        },
-        yaml = {
-            "block_mapping_pair"
-        }
+require("treesitter-context").setup({
+  enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+  max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+  trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+  min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+  patterns = {
+    -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
+    -- For all filetypes
+    -- Note that setting an entry here replaces all other patterns for this entry.
+    -- By setting the 'default' entry below, you can control which nodes you want to
+    -- appear in the context window.
+    default = {
+      "class",
+      "function",
+      "method",
+      "for",
+      "while",
+      "if",
+      "switch",
+      "case",
     },
-    exact_patterns = {},
-    -- [!] The options below are exposed but shouldn't require your attention,
-    --     you can safely ignore them.
+    -- Patterns for specific filetypes
+    -- If a pattern is missing, *open a PR* so everyone can benefit.
+    tex = {
+      "chapter",
+      "section",
+      "subsection",
+      "subsubsection",
+    },
+    rust = {
+      "impl_item",
+      "struct",
+      "enum",
+    },
+    scala = {
+      "object_definition",
+    },
+    vhdl = {
+      "process_statement",
+      "architecture_body",
+      "entity_declaration",
+    },
+    markdown = {
+      "section",
+    },
+    elixir = {
+      "anonymous_function",
+      "arguments",
+      "block",
+      "do_block",
+      "list",
+      "map",
+      "tuple",
+      "quoted_content",
+    },
+    json = {
+      "pair",
+    },
+    yaml = {
+      "block_mapping_pair",
+    },
+  },
+  exact_patterns = {},
+  -- [!] The options below are exposed but shouldn't require your attention,
+  --     you can safely ignore them.
 
-    zindex = 20, -- The Z-index of the context window
-    mode = "topline", -- Line used to calculate context. Choices: 'cursor', 'topline'
-    -- Separator between context and content. Should be a single character string, like '-'.
-    -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
-    separator = "-"
-}
+  zindex = 20, -- The Z-index of the context window
+  mode = "topline", -- Line used to calculate context. Choices: 'cursor', 'topline'
+  -- Separator between context and content. Should be a single character string, like '-'.
+  -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+  separator = "-",
+})
