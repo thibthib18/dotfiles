c0629287f34ca274863a0518e67d1392512dfdef
thib <thibault@seervision.com>
2024-02-28 11:21:29 +0400

    find hidden files

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -5,6 +5,11 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     opts = {
+      pickers = {
+        find_files = {
+          hidden = true,
+        },
+      },
       defaults = {
         file_ignore_patterns = { "venv/*", "tmux/plugins/*", "node_modules/*", "third_party", "vendor" },
       },
