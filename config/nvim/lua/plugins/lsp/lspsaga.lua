bd5f647225c158bafb623e8c21f102df91dc06d8
thib <thibault@seervision.com>
2024-02-28 19:08:41 +0400

    add better find references

--- config/nvim/lua/plugins/lsp/lspsaga.lua
+++ config/nvim/lua/plugins/lsp/lspsaga.lua
@@ -1,11 +1,20 @@
 return {
   "nvimdev/lspsaga.nvim",
-  config = function() require("lspsaga").setup({}) end,
+  config = function()
+    require("lspsaga").setup({
+      finder = {
+        keys = {
+          toggle_or_open = "<cr>",
+        },
+      },
+    })
+  end,
   dependencies = {
     "nvim-treesitter/nvim-treesitter", -- optional
     "nvim-tree/nvim-web-devicons", -- optional
   },
   keys = {
     { "<leader>rr", "<cmd>Lspsaga rename<cr>", desc = "Rename symbol under cursor" },
+    { "<leader>gr", "<cmd>Lspsaga finder<cr>", desc = "Find references to symbol under cursor" },
   },
 }
