fc3717a2400ddbe07283230234e92e456f131ed8
thib <thibault@seervision.com>
2024-02-28 14:09:23 +0400

    add correct Tab out config

--- config/nvim/lua/plugins/others.lua
+++ config/nvim/lua/plugins/others.lua
@@ -12,20 +12,19 @@ return {
 
   { "tpope/vim-repeat" },
 
-  -- opt
-  -- Tab out of (), needs treesitter
   {
     "abecodes/tabout.nvim",
+    lazy = false,
     config = function()
       require("tabout").setup({
         tabkey = "<Tab>", -- key to trigger tabout, set to an empty string to disable
         backwards_tabkey = "<S-Tab>", -- key to trigger backwards tabout, set to an empty string to disable
         act_as_tab = true, -- shift content if tab out is not possible
         act_as_shift_tab = false, -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
-        default_tab = "<C-t>", -- shift default action (only at the beginning of a line, otherwise <TAB> is {d)
+        default_tab = "<C-t>", -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
         default_shift_tab = "<C-d>", -- reverse shift default action,
         enable_backwards = true, -- well ...
-        completion = true, -- if the tabkey is {d in a completion pum
+        completion = false, -- if the tabkey is used in a completion pum
         tabouts = {
           { open = "'", close = "'" },
           { open = '"', close = '"' },
@@ -34,12 +33,25 @@ return {
           { open = "[", close = "]" },
           { open = "{", close = "}" },
         },
-        ignore_beginning = true --[[ if the cursor is at the beginning of a filled element it will rather tab out than shift the content ]],
+        ignore_beginning = true, --[[ if the cursor is at the beginning of a filled element it will rather tab out than shift the content ]]
         exclude = {}, -- tabout will ignore these filetypes
       })
     end,
-    wants = { "nvim-treesitter" }, -- or require if not {d so far
-    after = { "nvim-cmp" }, -- if a completion plugin is using tabs load it before
+    requires = {
+      "nvim-treesitter/nvim-treesitter",
+      "L3MON4D3/LuaSnip",
+      "hrsh7th/nvim-cmp",
+    },
+    opt = true, -- Set this to true if the plugin is optional
+    event = "InsertCharPre", -- Set the event to 'InsertCharPre' for better compatibility
+    priority = 1000,
+  },
+  {
+    "L3MON4D3/LuaSnip",
+    keys = function()
+      -- Disable default tab keybinding in LuaSnip
+      return {}
+    end,
   },
 
   -- Hard to { for now, will check it out later --> lets try the cousin plugin
