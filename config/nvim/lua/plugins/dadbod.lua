6310a9040708dd18479938c885d00a88e40d5f6f
thib <thibault@seervision.com>
2024-03-04 13:34:53 +0400

    set mongo database name

--- config/nvim/lua/plugins/dadbod.lua
+++ config/nvim/lua/plugins/dadbod.lua
@@ -6,10 +6,11 @@ return {
   },
   config = function()
     vim.g.db_ui_save_location = vim.fn.stdpath("config") .. "/db_ui"
-    local mongo_host = vim.env["MONGODB_HOST"]
-    local mongo_port = 27017
-    local mongo_connection_string = "mongodb://" .. mongo_host .. ":" .. mongo_port
     vim.g.dbs = { dop = mongo_connection_string }
+    local mongo_host = vim.env["MONGODB_HOST"] or "localhost"
+    local mongo_port = vim.env["MONGODB_EXTERNAL_PORT"] or 27017
+    local mongo_databse = vim.env["MONGODB_DATABASE"] or "local"
+    local mongo_connection_string = "mongodb://" .. mongo_host .. ":" .. mongo_port .. "/" .. mongo_databse
   end,
   keys = { { "<leader>dbt", vim.cmd.DBUIToggle, { desc = "Open database explorer" } } },
   cmd = { "DBUIToggle", "DBUI", "DBUIAddConnection", "DBUIFindBuffer", "DBUIRenameBuffer", "DBUILastQueryInfo" },
